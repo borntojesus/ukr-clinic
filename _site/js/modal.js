450ac8f653d51d7992934c72dc40d82ecb916dd3
dmitroantonuk <dmitry@antonyuk.org>
2022-12-10 15:16:33 +0200

    f

--- _site/js/modal.js
+++ _site/js/modal.js
@@ -1,14 +1,28 @@
-@import "variables";
-@import "normalize";
-@import "common";
-@import "home";
-@import "typography";
-@import "button";
-@import "blocks/card-blog";
-@import "blocks/modal";
-@import "blocks/card";
-@import "blocks/navigation";
-@import "blocks/tooltip";
-@import "blocks/footer";
-@import "blocks/star-rate";
+const modalWindow = document.getElementById('modal-one');
+const modalbutton = document.querySelector('[data-modal="modal-one"]');
+const modalOverlay = modalWindow.getElementsByClassName('modal__blur')[0];
+const modalHideButton = modalWindow.getElementsByClassName('modal__hide')[0];
+const bodyTag = document.getElementById('body');
+
+modalHideButton.addEventListener("click", hideModal);
+modalOverlay.addEventListener("click", hideModal)
+modalbutton.addEventListener("click", openModal);
+document.addEventListener('keydown', escKey);
+
+function openModal() {
+    modalWindow.classList.add("is-active");
+    bodyTag.classList.add("is-modal");
+}
+
+function hideModal() {
+    modalWindow.classList.remove("is-active");
+    bodyTag.classList.remove("is-modal");
+}
+
+function escKey(event) {
+    if(event.key === "Escape"){
+        hideModal();
+    }
+}
+
 
